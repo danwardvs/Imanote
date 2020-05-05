fd7c7469f8f01567466eef605224e93d50b0f872
Danny Van Stemp <danwardvs@adsgames.net>
2020-05-05 03:46:45 -0400

    loading characters from image

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ current_bit = ""
 bit_count = 0
 for i in range(0,width):
     for j in range(0,height):
-        r, g, b = rgb_im.getpixel((i, j))
+        r, g, b = rgb_im.getpixel((j, i))
+
         if b % 2 == 0:
             current_bit+='1'
         else:
             current_bit+='0'
         bit_count+=1
-        if bit_count == 7:
+        if bit_count == 8:
             bit_count = 0
-            print(current_bit)
+            print(int(current_bit,2))
             current_bit=""
+
 
         
